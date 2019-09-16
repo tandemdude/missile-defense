2732637f04828e3593f1e1feecf2b7c1890bcd1e
thomm.o <olijdthompson@gmail.com>
2019-09-16 08:41:48 +0000

    Refactor draw_lives() into its own updateable class

---
+++ lives.py
@@ -0,0 +1,26 @@
+import pygame
+import os
+
+
+class Lives:
+    def __init__(self, game_surface, screen_width, screen_height, font_size, lives):
+        self.game_surface = game_surface
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.font_size = font_size
+        self.lives = lives
+        self.font = self.font = pygame.font.Font(os.path.join("fonts", "SevenSegment.ttf"), 24)
+
+    def __int__(self):
+        return self.lives
+
+    def decrement(self):
+        self.lives -= 1 if self.lives > 0 else 0
+
+    def update(self):
+        text_surface = self.font.render(
+            f"Lives: {self.lives}", True, pygame.Color("#ffffff")
+        )
+        text_rect = text_surface.get_rect()
+        text_rect.topleft = (2, 2)
+        self.game_surface.blit(text_surface, text_rect)
