22f579ed4a825dedc679417c0014295a4623106e
thomm.o <olijdthompson@gmail.com>
2019-09-16 08:49:44 +0000

    Add missile graphic

--- missile.py
+++ missile.py
@@ -1,14 +1,23 @@
 import pygame
 import math
+import os
 
 SPRITE_WIDTH = 10
 SPRITE_HEIGHT = 10
-MISSILE_VELOCITY = 5
+MISSILE_VELOCITY = 7
 
 
 class Missile(pygame.sprite.Sprite):
     def __init__(self, game_surface, screen_width, screen_height, reticle_x, reticle_y):
         super().__init__()
+
+        self.asset = pygame.image.load(
+            os.path.join("images", "missile.png")
+        ).convert_alpha()
+        self.asset_width, self.asset_height = (
+            self.asset.get_width(),
+            self.asset.get_height(),
+        )
 
         self.game_surface = game_surface
         self.visible = True
@@ -17,12 +26,31 @@ class Missile(pygame.sprite.Sprite):
         self.end_x, self.end_y = reticle_x, reticle_y
         self.velocity_x, self.velocity_y = self.vector_from_positions()
 
-        self.image = pygame.Surface((SPRITE_WIDTH, SPRITE_HEIGHT))
-        self.image.fill(pygame.Color("#000000"))
-
-        pygame.draw.rect(
-            self.image, pygame.Color("#ff0000"), (0, 0, SPRITE_WIDTH, SPRITE_HEIGHT)
+        self.image = pygame.Surface(
+            (self.asset_width, self.asset_height), pygame.SRCALPHA
         )
+        self.image.blit(self.asset, (0, 0))
+        self.image = pygame.transform.scale(self.image, (25, 75))
+
+        angle = self.get_angle_positions()
+        print('angle:', angle * 180/math.pi)
+        self.image = pygame.transform.rotate(self.image, angle * 180/math.pi)
+
+        #self.image = pygame.Surface((SPRITE_WIDTH, SPRITE_HEIGHT))
+        #self.image.fill(pygame.Color("#000000"))
+
+        #pygame.draw.rect(
+        #    self.image, pygame.Color("#ff0000"), (0, 0, SPRITE_WIDTH, SPRITE_HEIGHT)
+        #)
+
+    def get_angle_positions(self):
+        y_diff = (self.end_y - self.y)
+        x_diff = (self.end_x - self.x)
+        if x_diff == 0:
+            angle = 0
+        else:
+            angle = math.pi/2 - math.atan(y_diff / x_diff)
+        return angle
 
     def vector_from_positions(self):
         velocity_x = (
