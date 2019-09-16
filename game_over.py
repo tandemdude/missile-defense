7619882c657db05473bbdbc8c5f4e41c83c7b634
thomm.o <olijdthompson@gmail.com>
2019-09-16 14:59:39 +0000

    Black file

--- game_over.py
+++ game_over.py
@@ -8,12 +8,12 @@ class GameOver:
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.font_size = font_size
-        self.font = self.font = pygame.font.Font(os.path.join("fonts", "SevenSegment.ttf"), self.font_size)
+        self.font = self.font = pygame.font.Font(
+            os.path.join("fonts", "SevenSegment.ttf"), self.font_size
+        )
 
     def update(self):
-        game_over_surface = self.font.render(
-            "GAME OVER", True, pygame.Color("#ff0000")
-        )
+        game_over_surface = self.font.render("GAME OVER", True, pygame.Color("#ff0000"))
         game_over_rect = game_over_surface.get_rect()
         game_over_rect.center = self.game_surface.get_rect().center
         self.game_surface.blit(game_over_surface, game_over_rect)
@@ -21,5 +21,8 @@ class GameOver:
             "Press [Y] to restart or [N] to quit", True, pygame.Color("#ff0000")
         )
         restart_msg_rect = restart_msg_surface.get_rect()
-        restart_msg_rect.center = (game_over_rect.center[0], game_over_rect.center[1] + self.font_size)
+        restart_msg_rect.center = (
+            game_over_rect.center[0],
+            game_over_rect.center[1] + self.font_size,
+        )
         self.game_surface.blit(restart_msg_surface, restart_msg_rect)
