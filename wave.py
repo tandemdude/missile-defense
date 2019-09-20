a44ac580418e6fb6153cdcdbcd7848d1ed1984ba
thomm.o <olijdthompson@gmail.com>
2019-09-20 11:09:14 +0000

    Change enemy to be visible until it touches the ground, then logic in the wave sets it to invisible once the lives have been decremented. Fixes all lives being lost at once bug as one enemy was having its hit_ground attribute checked multiple times.

--- wave.py
+++ wave.py
@@ -1,6 +1,7 @@
 import pygame
-from enemy import Enemy
+import random
 import os
+from enemy import Enemy
 
 
 class Wave:
@@ -16,16 +17,17 @@ class Wave:
         font_size
     ):
         self.number_of_enemies = number_of_enemies
-        self.time_limit = None
+        self.time_limit_in_frames = time_limit
         self.game_surface = game_surface
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.hit_ground_func = hit_ground_func
         self.enemies = []
         self.finished = False
-        self.register_enemies()
+        self.frames_since_start = 0
         self.num = wave_num + 1
         self.font = pygame.font.Font(os.path.join("fonts", "SevenSegment.ttf"), font_size)
+        self.enemy_spawn_times = [round(random.random() * self.time_limit_in_frames) for i in range(number_of_enemies)]
 
     def register_enemies(self):
         for _ in range(self.number_of_enemies):
@@ -38,6 +40,11 @@ class Wave:
             Enemy(self.game_surface, self.screen_width, self.screen_height)
         )
 
+    def register_new_enemy_if_required(self):
+        if self.frames_since_start in self.enemy_spawn_times:
+            for i in range(self.enemy_spawn_times.count(self.frames_since_start)):
+                self.register_enemy()
+
     def draw_wave_number(self):
         text_surface = self.font.render(
             f"Wave {self.num}", True, pygame.Color("#ffffff")
@@ -48,11 +55,19 @@ class Wave:
 
     def update(self):
         self.draw_wave_number()
+        self.register_new_enemy_if_required()
 
-        self.finished = True
-        for enemy in self.enemies:
-            enemy.update()
-            if enemy.hit_ground:
-                self.hit_ground_func()
-            if enemy.visible:
-                self.finished = False
+        if len(self.enemies) > 0 and len(self.enemies) <= self.number_of_enemies:
+            self.finished = True
+            for enemy in self.enemies:
+                enemy.update()
+                if enemy.hit_ground and enemy.visible:
+                    self.hit_ground_func()
+                    enemy.visible = False
+                if enemy.visible:
+                    self.finished = False
+
+        if len(self.enemies) != self.number_of_enemies:
+            self.finished = False
+
+        self.frames_since_start += 1
