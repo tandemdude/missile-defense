5c7a4c6325794a9614080b0633ba4e57f67c226b
thomm.o <olijdthompson@gmail.com>
2019-09-20 11:07:10 +0000

    Fix all lives being removed at once bug

--- enemy.py
+++ enemy.py
@@ -1,14 +1,24 @@
 import pygame
 import random
 import unittest
+import os
 
-SPRITE_WIDTH = 20
-SPRITE_HEIGHT = 20
+SPRITE_WIDTH = 25
+SPRITE_HEIGHT = 30
+VERTICAL_VELOCITY = 2
 
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, game_surface, screen_width, screen_height):
         super().__init__()
+
+        self.asset = pygame.image.load(
+            os.path.join("images", "enemy.png")
+        ).convert_alpha()
+        self.asset_width, self.asset_height = (
+            self.asset.get_width(),
+            self.asset.get_height(),
+        )
 
         self.screen_width = screen_width
         self.screen_height = screen_height
@@ -21,9 +31,19 @@ class Enemy(pygame.sprite.Sprite):
         self.image.fill(pygame.Color("#ffffff"))
         self.value = 150
 
-        pygame.draw.rect(
-            self.image, pygame.Color("#ff00ff"), (0, 0, SPRITE_WIDTH, SPRITE_HEIGHT)
+        self.image = pygame.Surface(
+            (self.asset_width, self.asset_height), pygame.SRCALPHA
         )
+        self.image.blit(self.asset, (0, 0))
+        self.image = pygame.transform.scale(self.image, (SPRITE_WIDTH, SPRITE_HEIGHT))
+
+        self.image = pygame.transform.rotate(self.image, self.get_angle_positions())
+
+    def get_angle_positions(self):
+        radius, angle = pygame.math.Vector2(
+            self.end_x - self.x, self.end_y - self.y
+        ).as_polar()
+        return -angle - 270
 
     def random_start_position(self):
         return (random.randint(0, self.screen_width - SPRITE_WIDTH), 0)
@@ -36,7 +56,7 @@ class Enemy(pygame.sprite.Sprite):
 
     def vector_from_positions(self):
         velocity_x = (self.end_x - self.x) / (self.end_y - self.y)
-        return (velocity_x, 1)
+        return (velocity_x, VERTICAL_VELOCITY)
 
     def generate_positions_and_velocities(self):
         self.x, self.y = self.random_start_position()
@@ -51,7 +71,6 @@ class Enemy(pygame.sprite.Sprite):
         if self.y >= self.screen_height - SPRITE_HEIGHT:
             self.velocity_y = 0
             self.hit_ground = True
-            self.visible = False
 
         if self.visible:
             self.move()
@@ -68,7 +87,7 @@ class EnemyTests(unittest.TestCase):
         e.end_x, e.end_y = (10, 10)
         x_vel, y_vel = e.vector_from_positions()
         self.assertEqual(x_vel, 0)
-        self.assertEqual(y_vel, 1)
+        self.assertEqual(y_vel, VERTICAL_VELOCITY)
 
     def test_velocity_from_positions_positive_x_difference(self):
         e = Enemy(None, 200, 100)
@@ -76,7 +95,7 @@ class EnemyTests(unittest.TestCase):
         e.end_x, e.end_y = (100, 10)
         x_vel, y_vel = e.vector_from_positions()
         self.assertEqual(x_vel, 10)
-        self.assertEqual(y_vel, 1)
+        self.assertEqual(y_vel, VERTICAL_VELOCITY)
 
     def test_velocity_from_positions_negative_x_difference(self):
         e = Enemy(None, 200, 100)
@@ -84,7 +103,7 @@ class EnemyTests(unittest.TestCase):
         e.end_x, e.end_y = (0, 10)
         x_vel, y_vel = e.vector_from_positions()
         self.assertEqual(x_vel, -10)
-        self.assertEqual(y_vel, 1)
+        self.assertEqual(y_vel, VERTICAL_VELOCITY)
 
 
 if __name__ == "__main__":
